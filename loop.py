6279115c560d47bf6588d0791edca967cc5aeae8
AkzTraL <147244175+AkzTraL@users.noreply.github.com>
2023-10-07 19:09:00 +0530

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,17 @@
+# For Loop
+
+fruits= ["apple", "banana", "mango" , "orange"]
+for fruit in fruits:
+  print(fruit)
+for i in fruit:
+  print(i)
+
+for k in range(3,31,4):
+  print(k)
+
+# While Loop
+
+i=0
+while (i<100):
+  print(i)
+  i= i+7
