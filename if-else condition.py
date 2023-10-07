185b0fbae1a7d1fd5c37ce475d588e162dd38f76
AkzTraL <147244175+AkzTraL@users.noreply.github.com>
2023-10-07 19:05:16 +0530

    Create if-else condition.py

---
+++ if-else condition.py
@@ -0,0 +1,23 @@
+h= int(input("Enter your height in cm:"))
+a= int(input("Enter your age"))
+if (h>180):
+  print("You are eligible to play basketball and football")
+
+elif (180>=h>=150):
+  print("You are eligible to play football")
+
+else:
+  print("You aren't eligible to play any sport")
+
+if (a>=18):
+  print("You are eligible to vote and have the permission to drive")
+
+else:
+  print("You are not eligible to vote and don't have the permission to drive")
+
+if (21>=a>=18):
+  print("You are eligible to appear for JEE")
+
+else:
+  print("you are not eligible to appear for JEE")
+  
